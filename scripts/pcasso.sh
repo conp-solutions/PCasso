abea2256bb51e25da6a6ca62a3ad832cb75a4dda
Norbert Manthey <norbert.manthey@googlemail.com>
2017-10-20 21:48:23 +0200

    pcasso: drop options in script
    As the default options of pcasso have been set to the SC2014 options now,
    there is no need to re-set them in the script again. Hence, drop the
    options that would set default values.

--- scripts/pcasso.sh
+++ scripts/pcasso.sh
@@ -9,7 +9,7 @@
 threads=12
 child_nodes=7
 # all parameters for pcasso
-PCASSO_PARAM="-model -work-conflicts=-1 -work-timeout=-1 -split-mode=2 -split-timeout=1024 -presel-fac=0.1 -presel-min=64 -presel-max=1024 -fail-lit=2 -nec-assign=2 -num-iterat=3 -double-la -double-decay=0.95 -con-resolv=1 -bin-const -la-heur=4 -presel-heur=2 -clause-learn=2 -dir-prior=3 -child-count=$child_nodes -shrk-clause -var-eq=3 -split-method=1 -split-depth=0 -dseq -h-acc=3 -h-maxcl=7 -h-cl-wg=5 -h-upper=10900 -h-lower=0.1 -addcl-falserem=1 -shpool-size=15000 -shclause-size=2 -lbd-lt=2 -dyn-lbdsh -dyn-lbdshfac=0.5 -every-shpool -sh-delay=16 -receiver-filter=0 -stop-children -adp-presel -adp-preselL=50 -adp-preselS=7 -upd-actpol=2 -sort-split -tabu -pure-lit=2 -no-split-diver -spdiver-lim=4096 -satunsat-restart -learnt-worsening -learnt-unaryres -clean-delay=2 -sim-port -c-killing -load-split -threads=$threads"
+PCASSO_PARAM="-model -child-count=$child_nodes -threads=$threads"
 
 # default parameters for preprocessor
 cpParams="-enabled_cp3 -cp3_stats -bve -bve_red_lits=1 -fm -no-cp3_fm_vMulAMO -unhide -cp3_uhdIters=5 -cp3_uhdEE -cp3_uhdTrans -bce -bce-cle -no-bce-bce -dense"
