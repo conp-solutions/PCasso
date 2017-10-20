dd09bc3dcbb93ebc62275e742a5fa0e4f8d0bbae
Norbert Manthey <norbert.manthey@googlemail.com>
2017-10-20 15:11:21 +0200

    pcasso: make pcasso.sh robust
    The current version of the script is more robust and expects the pcasso and
    coprocessor binary in the current directory.

--- scripts/pcasso.sh
+++ scripts/pcasso.sh
@@ -1,32 +1,58 @@
 #!/bin/bash
-# pcasso.sh, Norbert Manthey, 2014
+# pcasso.sh, Norbert Manthey, 2014 - 2017
 #
-# solve CNF formula $1 by simplifying first with coprocessor, then run a SAT solver, and finally, reconstruct the model
+# solve CNF formula $1 by simplifying first with coprocessor, then run the pcasso SAT solver, and finally, reconstruct the model
 # $2 points to the temporary directory (no trailing /)
 #
 
-# binary of the used SAT solver
-satsolver=./pcasso						# name of the binary (if not in this directory, give full path)
+# pcasso parameters (some out of many)
+threads=12
+child_nodes=7
+# all parameters for pcasso
+PCASSO_PARAM="-model -work-conflicts=-1 -work-timeout=-1 -split-mode=2 -split-timeout=1024 -presel-fac=0.1 -presel-min=64 -presel-max=1024 -fail-lit=2 -nec-assign=2 -num-iterat=3 -double-la -double-decay=0.95 -con-resolv=1 -bin-const -la-heur=4 -presel-heur=2 -clause-learn=2 -dir-prior=3 -child-count=$child_nodes -shrk-clause -var-eq=3 -split-method=1 -split-depth=0 -dseq -h-acc=3 -h-maxcl=7 -h-cl-wg=5 -h-upper=10900 -h-lower=0.1 -addcl-falserem=1 -shpool-size=15000 -shclause-size=2 -lbd-lt=2 -dyn-lbdsh -dyn-lbdshfac=0.5 -every-shpool -sh-delay=16 -receiver-filter=0 -stop-children -adp-presel -adp-preselL=50 -adp-preselS=7 -upd-actpol=2 -sort-split -tabu -pure-lit=2 -no-split-diver -spdiver-lim=4096 -satunsat-restart -learnt-worsening -learnt-unaryres -clean-delay=2 -sim-port -c-killing -load-split -threads=$threads"
+
+# default parameters for preprocessor
+cpParams="-enabled_cp3 -cp3_stats -bve -bve_red_lits=1 -fm -no-cp3_fm_vMulAMO -unhide -cp3_uhdIters=5 -cp3_uhdEE -cp3_uhdTrans -bce -bce-cle -no-bce-bce -dense"
 
 #
 # usage
 #
 if [ "x$1" = "x" ]; then
-  echo "USAGE: cp.sh <input CNF> [arguments for cp3]"
+  echo "USAGE: pcasso.sh <input CNF> [<tmpDir> [arguments for cp3]]"
   exit 1
 fi
+
+# check whether all tools are present
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ ! -x "$DIR/pcasso" ]
+then
+	echo "error: solver $DIR/pcasso could not be found, abort!"
+	exit 1
+fi
+if [ ! -x "$DIR/coprocessor" ]
+then
+	echo "error: coprocessor $DIR/coprocessor could not be found, abort!"
+	exit 1
+fi
+solver="$DIR/pcasso"
+simplifier="$DIR/coprocessor"
 
 #
 # variables for the script
 #
+file=$1				# first argument is CNF instance to be solved
+shift 1
+tmpDir=$2			# directory to write the temporary files to
+shift 1											# reduce the parameters, removed the very first one. remaining $@ parameters are arguments
 
-file=$1											# first argument is CNF instance to be solved
-shift												# reduce the parameters, removed the very first one. remaining $@ parameters are arguments
-tmpDir=$1										# directory to write the temporary files to
-shift												# reduce the parameters, removed the very first one. remaining $@ parameters are arguments
+# in case no tmpDir has been specified, create one and trap for deletion on exit
+if [ -z "$tmpDir" ]
+then
+	tmpDir=$(mktemp -d)
+	[ -z "$tmpDir" ] || trap "rm -rf $tmpDir" EXIT
+fi
 
-# default parameters for preprocessor
-cpParams="-enabled_cp3 -cp3_stats -bve -bve_red_lits=1 -fm -no-cp3_fm_vMulAMO -unhide -cp3_uhdIters=5 -cp3_uhdEE -cp3_uhdTrans -bce -bce-cle -no-bce-bce -dense"
+
 
 # some temporary files 
 undo=$tmpDir/undo_$$						# path to temporary file that stores cp3 undo information
@@ -45,8 +71,7 @@ solveEnd=0
 # and output to stdout of the preprocessor is redirected to stderr
 #
 ppStart=`date +%s`
-#echo "call: ./coprocessor $file $realModel -enabled_cp3 -undo=$undo -dimacs=$tmpCNF $cpParams $@"
-./coprocessor $file $realModel -enabled_cp3 -undo=$undo -dimacs=$tmpCNF $cpParams $@  1>&2
+$simplifier $file $realModel -enabled_cp3 -undo=$undo -dimacs=$tmpCNF $cpParams $@  1>&2
 exitCode=$?
 ppEnd=`date +%s`
 echo "c preprocessed $(( $ppEnd - $ppStart)) seconds with exit code $exitCode" 1>&2
@@ -71,7 +96,7 @@ else
 		# and output to stdout of the sat solver is redirected to stderr
 		#
 		solveStart=`date +%s`
-		$satsolver $tmpCNF $model -model -work-conflicts=-1 -work-timeout=-1 -split-mode=2 -split-timeout=1024 -presel-fac=0.1 -presel-min=64 -presel-max=1024 -fail-lit=2 -nec-assign=2 -num-iterat=3 -double-la -double-decay=0.95 -con-resolv=1 -bin-const -la-heur=4 -presel-heur=2 -clause-learn=2 -dir-prior=3 -child-count=7 -shrk-clause -var-eq=3 -split-method=1 -split-depth=0 -dseq -h-acc=3 -h-maxcl=7 -h-cl-wg=5 -h-upper=10900 -h-lower=0.1 -addcl-falserem=1 -shpool-size=15000 -shclause-size=2 -lbd-lt=2 -dyn-lbdsh -dyn-lbdshfac=0.5 -every-shpool -sh-delay=16 -receiver-filter=0 -stop-children -adp-presel -adp-preselL=50 -adp-preselS=7 -upd-actpol=2 -sort-split -tabu -pure-lit=2 -no-split-diver -spdiver-lim=4096 -satunsat-restart -learnt-worsening -learnt-unaryres -clean-delay=2 -sim-port -c-killing -load-split -threads=11  1>&2
+		$solver $tmpCNF $model $PCASSO_PARAM  1>&2
 		exitCode=$?
 		solveEnd=`date +%s`
 		echo "c solved $(( $solveEnd - $solveStart )) seconds, exit code: $exitCode" 1>&2
@@ -82,16 +107,8 @@ else
 		# coprocessor can also handle "s UNSATISFIABLE"
 		#
 		echo "c post-process with cp3" 1>&2
-		./coprocessor -post -undo=$undo -model=$model $cpParams > $realModel
-	
-		#
-		# verify final output if SAT?
-		#
-		if [ "$exitCode" -eq "10" ]
-		then
-			echo "c verify model ..." 1>&2
-#			./verify SAT $realModel $file
-		fi
+		$simplifier -post -undo=$undo -model=$model $cpParams > $realModel
+
 	else
 		#
 		# preprocessor returned some unwanted exit code
@@ -102,7 +119,7 @@ else
 		# and output to stdout of the sat solver is redirected to stderr
 		#
 		solveStart=`date +%s`
-		$satsolver $file $realModel -model -work-conflicts=-1 -work-timeout=-1 -split-mode=2 -split-timeout=1024 -presel-fac=0.1 -presel-min=64 -presel-max=1024 -fail-lit=2 -nec-assign=2 -num-iterat=3 -double-la -double-decay=0.95 -con-resolv=1 -bin-const -la-heur=4 -presel-heur=2 -clause-learn=2 -dir-prior=3 -child-count=7 -shrk-clause -var-eq=3 -split-method=1 -split-depth=0 -dseq -h-acc=3 -h-maxcl=7 -h-cl-wg=5 -h-upper=10900 -h-lower=0.1 -addcl-falserem=1 -shpool-size=15000 -shclause-size=2 -lbd-lt=2 -dyn-lbdsh -dyn-lbdshfac=0.5 -every-shpool -sh-delay=16 -receiver-filter=0 -stop-children -adp-presel -adp-preselL=50 -adp-preselS=7 -upd-actpol=2 -sort-split -tabu -pure-lit=2 -no-split-diver -spdiver-lim=4096 -satunsat-restart -learnt-worsening -learnt-unaryres -clean-delay=2 -sim-port -c-killing -load-split -threads=12 1>&2
+		$solver $file $realModel $PCASSO_PARAM 1>&2
 		exitCode=$?
 		solveEnd=`date +%s`
 		echo "c solved $(( $solveEnd - $solveStart )) seconds" 1>&2
